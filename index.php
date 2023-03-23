df0908c454db229445194a2b2277fa5a69e70fcf
Thomas-Basadonne <thommybasa@gmail.com>
2023-03-23 19:05:18 +0100

    creazione file index + models

---
+++ index.php
@@ -0,0 +1,26 @@
+<!-- Immaginare quali sono le classi necessarie per creare uno shop online con le seguenti caratteristiche:
+L'e-commerce vende **prodotti** per animali.
+I prodotti sono categorizzati, le **categorie** sono Cani o Gatti.
+I prodotti saranno oltre al **cibo**, anche **giochi**, **cucce**, etc.
+Stampiamo delle card contenenti i dettagli dei prodotti, come immagine, titolo, prezzo, icona della categoria ed il tipo di articolo che si sta visualizzando (prodotto, cibo, gioco, cuccia). -->
+<?php
+include_once "./models/Categoria.php";
+include_once "./models/Prodotto.php";
+include_once "./models/Cibo.php";
+include_once "./models/Giochi.php";
+include_once "./models/Cuccia.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-commerce Pet</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+<body>
+    
+</body>
+</html>
